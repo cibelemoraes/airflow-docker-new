61913efab87e4003699086925a5b2e344eca0bd4
Your Name <you@example.com>
2024-08-24 20:43:26 -0300

    conexões gcp

--- dags/dag_parquet_e_lotes.py
+++ dags/dag_parquet_e_lotes.py
@@ -1,7 +1,5 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.decorators import dag, task
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
@@ -19,54 +17,138 @@ from cosmos.config import ProjectConfig, RenderConfig
 
 from airflow.models.baseoperator import chain
 
-# Importando as funções dos arquivos externos
-from utils.download_parquet import download_parquet_from_url
-from utils.manipular_parquet import manipular_parquet
-from utils.verificar_novos_dados import verificar_novos_dados
-from utils.carregar_no_postgresql import load_data_from_postgres_to_bigquery
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 8, 14),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+@dag(
+    start_date=datetime(2024, 8, 24),
+    schedule=None,
+    catchup=False,
+    tags=['retail'],
+)
+def retail():
+    bucket_name = 'pedrapagamentos'
+    @task.external_python(python='/usr/local/airflow/pandas_venv/bin/python')
+    def correct_csv_format():
+        import pandas as pd
 
-with DAG(
-    'dag_parquet_e_lotes',
-    default_args=default_args,
-    description='DAG que baixa Parquet e processa lotes pendentes a cada 1 hora',
-    schedule_interval=timedelta(hours=1),
-    catchup=False
-) as dag:
+        file_path = 'include/datasets/online_retail.csv'
+        new_file_path = 'include/datasets/online_retail_dataset.csv'
+        df = pd.read_csv(file_path, encoding='ISO-8859-1')
+        df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'], format='%m/%d/%y %H:%M', errors='coerce')
+        df.to_csv(new_file_path, index=False)
+    
 
-    baixar_parquet_task = PythonOperator(
-        task_id='baixar_parquet',
-        python_callable=download_parquet_from_url,
-        op_kwargs={
-            'bucket_name': 'desafio-eng-dados',
-            'source_blob_name': '2024-03-06.pq',
-            'destination_file_name': '/tmp/2024-03-06.pq'
+    upload_retail_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_retail_csv_to_gcs',
+        src='include/datasets/online_retail_dataset.csv',
+        dst='raw/online_retail.csv',
+        bucket=bucket_name,
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+
+    upload_country_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_country_csv_to_gcs',
+        src='include/datasets/country.csv',
+        dst='raw/country.csv',
+        bucket=bucket_name,
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    retail_gcs_to_raw = aql.load_file(
+        task_id='retail_gcs_to_raw',
+        input_file=File(
+            f'gs://{bucket_name}/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=True,
+        native_support_kwargs={
+            "encoding": "ISO_8859_1",
         }
     )
 
-    manipular_parquet_task = PythonOperator(
-        task_id='manipular_parquet',
-        python_callable=manipular_parquet,
-        op_kwargs={'file_path': '/tmp/2024-03-06.pq'}
+    country_gcs_to_raw = aql.load_file(
+        task_id='country_gcs_to_raw',
+        input_file=File(
+            f'gs://{bucket_name}/raw/country.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_country',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=True,
+        native_support_kwargs={
+            "encoding": "ISO_8859_1",
+        }
     )
 
-    verificar_novos_dados_task = PythonOperator(
-        task_id='verificar_novos_dados',
-        python_callable= verificar_novos_dados
-    )
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
     
-     carregar_no_postgresql_task = PythonOperator(
-        task_id='load_data_from_postgres_to_bigquery',
-        python_callable= load_data_from_postgres_to_bigquery
+    
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
     )
 
-    baixar_parquet_task >> manipular_parquet_task >> processar_lotes_task >> extrair_dados_task >> transformar_dados_task >> carregar_dados_task
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    chain(
+        correct_csv_format(),
+        upload_retail_csv_to_gcs,
+        upload_country_csv_to_gcs,
+        create_retail_dataset,
+        retail_gcs_to_raw,
+        country_gcs_to_raw,
+        check_load(),
+        transform,
+        check_transform(),
+        report,
+        check_report()
+    )
+
+retail()
